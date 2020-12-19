442d8e09d81cf7a9e671ada8baa07782d6a3760b
remdd <robin.ed.marsden@gmail.com>
2020-12-19 13:17:37 +0000

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from 'react'
+import Head from 'next/head'
+import io from 'socket.io-client'
+import cx from 'classnames'
+import ScaleText from 'react-scale-text'
+
+export default function Home() {
+  const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL
+  const [data, setData] = useState({})
+  const [id, setId] = useState('')
+
+  useEffect(() => {
+    const socket = io(SERVER_URL)
+
+    socket.on('connect', () => {
+      setId(socket.id)
+      console.log(`connected: ${socket.id}`)
+    })
+
+    socket.on('disconnect', () => {
+      console.log(`disconnected: ${socket.id}`)
+    })
+
+    socket.on('data', (newData) => {
+      console.log(newData)
+      setData(newData)
+      console.log(data)
+    })
+  }, [])
+
+  return (
+    <div>
+      <Head>
+        <title>Vizlizer2 client</title>
+      </Head>
+
+      <main
+        className={cx(
+          "w-screen h-screen bg-greenscreen overflow-hidden"
+        )}
+      >
+        {/* <pre>
+          Socket ID: {id}
+        </pre>
+        <pre>
+          {JSON.stringify(data)}
+        </pre> */}
+
+        <div className="w-full h-full text-center">
+          <ScaleText>
+            {(data.text1 || data.text2) ? (
+              <div className="flex justify-center h-full">
+                <span
+                  className={cx(
+                    `font-${data.font}`,
+                    "uppercase flex flex-col justify-center align-middle leading-none p-32",
+                    `${data.clientClass}`,
+                    `fontColor-${data.fontColor}`,
+                  )}
+                  style={{
+                    WebkitTextStrokeWidth:
+                      (data.font === 'bungee' ||
+                      data.font === 'freckle-face') &&
+                      '5px',
+                    WebkitTextStrokeColor: 'white'
+                  }}
+                >
+                  {data.text1 ? data.text1 : data.text2}
+                </span>
+              </div>
+            ) : (
+              <span className="opacity-0 font-tiny">.</span>
+            )}
+          </ScaleText>
+        </div>
+      </main>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  // purge: ['./pages/**/*.js', './components/**/*.js'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      greenscreen: '#00FF00',
+      blue: 'blue',
+    },
+    fontSize: {
+      'huge': '20rem',
+      'tiny': '1px',
+    },
+    fontFamily: {
+      'bungee': ['Bungee'],
+      'special-elite': ['Special Elite'],
+      'bowlby-one': ['Bowlby One'],
+      'freckle-face': ['Freckle Face'],
+    },
+  },
+  variants: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
